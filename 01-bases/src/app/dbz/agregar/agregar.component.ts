8d944bb0e9a308222eaad45f04b547232c7f4220
Marcelo Oroquieta <marcelo@oroquieta.com>
2021-04-29 21:15:02 -0300

    Angular: Formularios, @Input @Output EventEmitter

---
+++ 01-bases/src/app/dbz/agregar/agregar.component.ts
@@ -0,0 +1,30 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interface';
+
+@Component({
+    selector: 'app-agregar',
+    templateUrl: './agregar.component.html',
+})
+export class AgregarComponent {
+    
+    @Input() nuevoPersonaje: Personaje = {
+        nombre: '',
+        poder: 0
+    }
+
+    @Output() onNuevoPersonaje: EventEmitter<Personaje> = new EventEmitter();
+
+    agregar() {    
+        if (this.nuevoPersonaje.nombre.trim().length === 0) {
+            return;
+        }
+
+        this.onNuevoPersonaje.emit(this.nuevoPersonaje);
+        
+        this.nuevoPersonaje = {
+            nombre: '',
+            poder: 0
+        }
+    }
+
+}
